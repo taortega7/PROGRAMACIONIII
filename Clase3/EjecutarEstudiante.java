bab10d1065d5fb8025befa7a0a37bbe8595b6266
Tati <taortega@estudiante.uniajc.edu.co>
2025-08-26 15:44:42 +0000

    Actividad independeinte

--- Clase3/EjecutarEstudiante.java
+++ Clase3/EjecutarEstudiante.java
@@ -13,14 +13,14 @@ public class EjecutarEstudiante {
         e[0] = objEst1;
         e[1] = objEst2;
         e[2] = objEst3;
-        e[3] = new Estudiante(4, 1008940941, "Peter", "Ingeniería");
+        e[3] = new Estudiante(4, 1663405777, "Tatiana", "Ingeniería");
 
-        // === Actividad independiente ===
+        //*** Actividad independiente ***
         // Matricular cursos a cada estudiante
         objEst1.matricularCursos(new String[] { "Programación", "Matemáticas", "Bases de Datos" });
         objEst2.matricularCursos(new String[] { "Física", "Cálculo", "Algoritmos" });
         objEst3.matricularCursos(new String[] { "Anatomía", "Biología", "Química" });
-        e[3].matricularCursos(new String[] { "Ética", "Redes", "Inteligencia Artificial" });
+        e[3].matricularCursos(new String[] { "Matematicas", "Redes", "Ingenieria De Software" });
 
         // Imprimir todos los estudiantes con sus cursos
         System.out.println("\n--- Lista de estudiantes con sus cursos ---");
