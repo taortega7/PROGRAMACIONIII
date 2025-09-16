ae31b1596cba570697e49811257423a7658ba751
Tati <taortega@estudiante.uniajc.edu.co>
2025-09-16 16:55:08 +0000

    Clase 7

---
+++ Clase7/OperacionesPila.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class OperacionesPila {
+
+    Stack<String> pilaSignos;
+
+    public boolean validarParentesis(String [] s){
+        boolean flag = false;
+        
+        pilaSignos = new Stack<>();
+
+        //ALMACENAR EN LA PILA EL ARREGLO DE SIGNOS 
+        for (int i = 0; i < s.length; i++) {
+            pilaSignos.push(s[i]);
+    }
+    int tam = pilaSignos.size();
+    int cont1 = 0; cont2 = 0; 
+
+    //RECORRER LA PILA 
+    for (int i = 0; i < tam; i++) {
+       pilaSignos.peek() .equals("("){
+        cont1 += 1;
+         } else {
+            cont2 += 1;
+         }
+       }
+
+       flag = (cont1 == cont2 ? true : false);
+
+
+
+    return flag;
+    }
+}
--- Clase7/PilaS441.java
+++ Clase7/PilaS441.java
@@ -1,36 +1,42 @@
-import java.util.Stack; 
+import java.util.Stack;
 
-public class PilaS441 { 
+public class PilaS441 {
 
     public static void main(String[] args) {
 
-        //CREACION DE UNA PILA DE NUM ENTEROS 
+        // CREACION DE UNA PILA DE NUM ENTEROS
         Stack<Integer> pila = new Stack<>();
 
-        //INSERTAR ELEMENTOS EN LA PILA
-    pila.push(8);
-    pila.push(4);
-    pila.push(9);
-    pila.push(10);
-    pila.push(6);
+        // VERIFICAR SI LA PILA ESTA VACIA
+        System.out.println(pila.empty()); // true
 
-        //IMPRIMIR LA PILA
+        // INSERTAR ELEMENTOS EN LA PILA
+        pila.push(8);
+        pila.push(4);
+        pila.push(9);
+        pila.push(10);
+        pila.push(6);
+
+        // IMPRIMIR LA PILA
         System.out.println(pila);
 
-        //MOSTRAR EL ELEMENTO TOPE DE LA PILA
+        // MOSTRAR EL ELEMENTO TOPE DE LA PILA
         System.out.println("Tope de la pila: " + pila.peek());
 
-        //ELIMINAR EL ELEMENTO TOPE DE LA PILA
+        // ELIMINAR EL ELEMENTO TOPE DE LA PILA
         System.out.println("Elemento removido del tope de la pila: " + pila.pop());
 
-        //IMPRIMIR LA PILA
+        // IMPRIMIR LA PILA
         System.out.println(pila);
 
         // BUSCAR Y MOSTRAR LA POSICION DE UN ELEMENTO EN LA PILA
-        System.out.println(pila.search(10)); 
-        System.out.println(pila.search(9)); 
-        System.out.println(pila.search(4)); 
-        System.out.println(pila.search(8)); 
-        System.out.println(pila.search(50)); 
+        System.out.println(pila.search(10));
+        System.out.println(pila.search(9));
+        System.out.println(pila.search(4));
+        System.out.println(pila.search(8));
+        System.out.println(pila.search(50));
+
+        // VERIFICAR SI LA PILA ESTA VACIA
+        System.out.println(pila.empty()); // false
     }
 }
